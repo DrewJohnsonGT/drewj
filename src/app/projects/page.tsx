2bd239d930a36531450b8d86ab2f49a865ef4b2d
Drew Johnson <drewjohnsongt@gmail.com>
2024-06-28 23:56:57 -0400

    WIP for projects until content is filled

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -17,46 +17,49 @@ const ProjectsPage = async () => {
   const projects = await getAllProjectsFrontMatter();
 
   return (
-    <SimpleGrid minChildWidth="300px" spacing="2rem" m={4}>
-      {projects.map((project) => (
-        <Card maxW="xxl" key={project.title} cursor="pointer">
-          <CardBody>
-            <Image
-              src={project.thumbnail}
-              alt={project.title}
-              borderRadius="lg"
-            />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">{project.title}</Heading>
-              <Text>{project.description}</Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <ButtonGroup spacing="2">
-              {project.repository && (
-                <Button
-                  as="a"
-                  href={project.repository}
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  Repository
-                </Button>
-              )}
-              {project.link && (
-                <Button
-                  as="a"
-                  href={project.link}
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  Visit
-                </Button>
-              )}
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
-      ))}
-    </SimpleGrid>
+    <>
+      WIP - filling out this content soon
+      <SimpleGrid minChildWidth="300px" spacing="2rem" m={4}>
+        {projects.map((project) => (
+          <Card maxW="xxl" key={project.title} cursor="pointer">
+            <CardBody>
+              <Image
+                src={project.thumbnail}
+                alt={project.title}
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">{project.title}</Heading>
+                <Text>{project.description}</Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <ButtonGroup spacing="2">
+                {project.repository && (
+                  <Button
+                    as="a"
+                    href={project.repository}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Repository
+                  </Button>
+                )}
+                {project.link && (
+                  <Button
+                    as="a"
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Visit
+                  </Button>
+                )}
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
 
