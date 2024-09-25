e0a8478bdbde96215f71aad2fd3c6545efa6d2a8
Drew Johnson <DrewJohnsonGT@gmail.com>
2024-09-25 11:57:42 -0400

    Update page.tsx

--- src/app/misc/timer/page.tsx
+++ src/app/misc/timer/page.tsx
@@ -20,7 +20,7 @@ const TIMES = [
   },
   {
     goal: ONE_WEEK,
-    time: new Date('Sep 24 2024 16:07:00 GMT-0400 (Eastern Daylight Time)'),
+    time: new Date('Sep 25 2024 11:30:00 GMT-0400 (Eastern Daylight Time)'),
   },
 ];
 
