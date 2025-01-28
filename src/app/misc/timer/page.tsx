b1d7503c61746137209e3f2e786b8941880cbc8b
Drew Johnson <drewjohnsongt@gmail.com>
2025-01-28 10:44:42 -0500

    Last reset

--- src/app/misc/timer/page.tsx
+++ src/app/misc/timer/page.tsx
@@ -19,7 +19,7 @@ const ONE_MONTH = ONE_WEEK * 4;
 const TIMES = [
   {
     goal: ONE_MONTH,
-    time: new Date('Jan 26 2025 15:05:00 GMT-0500 (Eastern Standard Time)'),
+    time: new Date('Jan 28 2025 10:42:00 GMT-0500 (Eastern Standard Time)'),
   },
 ];
 
