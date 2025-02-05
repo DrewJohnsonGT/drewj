7c7fb0ecd0f47072cea95665aacb494376d63d70
Drew Johnson <DrewJohnsonGT@gmail.com>
2025-02-05 15:52:16 -0500

    Update page.tsx

--- src/app/misc/timer/page.tsx
+++ src/app/misc/timer/page.tsx
@@ -19,7 +19,7 @@ const ONE_MONTH = ONE_WEEK * 4;
 const TIMES = [
   {
     goal: ONE_MONTH,
-    time: new Date('Jan 28 2025 13:57:00 GMT-0500 (Eastern Standard Time)'),
+    time: new Date('Feb 5th 2025 15:50:00 GMT-0500 (Eastern Standard Time)'),
   },
 ];
 
