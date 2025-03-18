0a28fc3d00b661451ab6e6d1fd069b4d903fd163
Drew Johnson <drewjohnsongt@gmail.com>
2025-03-18 15:03:54 -0400

    Fix static page build issue

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import BundleAnalyzer from '@next/bundle-analyzer';
 
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ['next-mdx-remote'], // Should be resolved in future releases https://github.com/vercel/next.js/issues/76395#issuecomment-2721048784
+};
 
 const withBundleAnalyzer = BundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
