3e0247a35b1c4a7937b57a39c1d23d0960cc8c12
Drew Johnson <drewjohnsongt@gmail.com>
2025-05-24 15:50:46 -0400

    Reset

--- src/app/misc/timer/page.tsx
+++ src/app/misc/timer/page.tsx
@@ -18,7 +18,7 @@ const ONE_MONTH = ONE_WEEK * 4;
 
 const TIME = {
   goal: ONE_MONTH,
-  time: new Date('Wed May 21 2025 14:24:00 GMT-0400 (Eastern Daylight Time)'),
+  time: new Date('Sat May 24 2025 15:44:00 GMT-0400 (Eastern Daylight Time)'),
 };
 
 const getPercentOfGoal = (date: Date, goal: number) => {
